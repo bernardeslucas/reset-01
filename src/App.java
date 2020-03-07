989d1922198ba82b0d47d8ceacd57642fc733516
lucasbe1909 <lucasbernardess95@gmail.com>
2020-03-07 15:30:45 -0300

    Calculadora

--- src/App.java
+++ src/App.java
@@ -1,14 +1,23 @@
 public class App {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("alterado");
+
+        Calculadora calculadoraDeIdades = new Calculadora();
+
+        int somaDasIdades = calculadoraDeIdades.soma(31, 41);
+        System.out.println(somaDasIdades);
+
+        int subtraiIdades = calculadoraDeIdades.subtrai(40, 30);
+        System.out.println(subtraiIdades);
+
+        int multiplicaIdades = calculadoraDeIdades.multiplica(40, 3);
+        System.out.println(multiplicaIdades);
+
+        int divideIdades = calculadoraDeIdades.divide(40, 30);
+        System.out.println(divideIdades);
 
 
     }
-
-
 
 
 }
---
+++ src/Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+
+    public int soma(int valorUm, int valorDois) {
+        return valorUm + valorDois;
+    }
+
+    public int subtrai(int valorUm, int valorDois) {
+        return valorUm - valorDois;
+    }
+
+    public int multiplica(int valorUm, int valorDois) {
+        return valorUm * valorDois;
+    }
+
+    public int divide(int valorUm, int valorDois) {
+        return valorUm / valorDois;
+    }
+
+}
